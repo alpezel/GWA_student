213686040dd9326916c1b6b1bfdb8a7f0064df73
alpezel <allistairevillaruel@gmail.com>
2023-04-27 17:16:00 +0800

    input 20 names of student with GWA and open the students_gwa.txt file

---
+++ GWA_student.py
@@ -0,0 +1,7 @@
+#Write a Python program that read a file containing the name of 20 students
+#together with their GWA. The program will outputs the name of the student 
+#who got the highest GWA (including the GWA).
+
+# open students_gwa.txt (read)
+with open("students_gwa.txt") as students_gwa_file:
+    
